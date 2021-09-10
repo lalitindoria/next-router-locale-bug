cf7c7a5286e974da85a2bbcbe4c3283e844c8992
Lalit Indoria <lalit@clearfeed.ai>
2021-09-10 19:00:59 +0000

    Test route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+    localeDetection: false,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/test",
+        permanent: false,
+        locale: false,
+      },
+    ];
+  },
+};
